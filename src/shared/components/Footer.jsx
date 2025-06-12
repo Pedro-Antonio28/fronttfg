527436afba6f74edf2c80c484828069926471143
Pedro Antonio <1919952@alu.murciaeduca.es>
2025-06-12 15:23:34 +0200

    Feat: Creator exam

--- src/shared/components/Footer.jsx
+++ src/shared/components/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="bg-white text-center py-3 text-sm text-gray-500">
+  <footer className="bg-white text-center py-3 text-sm text-gray-500 dark:bg-gray-900 p-10 space-y-12" >
     © {new Date().getFullYear()} Ludus — Todos los derechos reservados.
   </footer>
 );
